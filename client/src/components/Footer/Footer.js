2787e17336e31cb450ef3d7e2c3e34431b3ece23
aidanclemente <aidanclemente@gmail.com>
2018-04-26 20:26:00 -0400

    erging with master

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,49 +5,49 @@ import "./Footer.css";
   render() {
     return (
         
-  <footer class="page-footer text-center mt-4 wow fadeIn">
+  <footer className="page-footer text-center mt-4 wow fadeIn">
 
     {/*Call to action  */}
-    <div class="pt-2">
+    <div className="pt-2">
       <p id="footerConnect">Connect With Us</p>
     </div>
     {/*/.Call to action  */}
 
     {/* Optional spacing */}
-    {/* <div class="container">
-      <hr class="my-2 mx-2"></hr>
+    {/* <div className="container">
+      <hr className="my-2 mx-2"></hr>
     </div> */}
     
     {/* Social icons   */}
-    <div class="icons">
+    <div className="icons">
     
       {/* Facebook Update href */}
       <a href="https://www.facebook.com/greentablefarm/" target="_blank">
-        <i class="icon fa fa-facebook mr-3"></i>
+        <i className="icon fa fa-facebook mr-3"></i>
       </a>
 
       {/*  Email Update href */}
-      <a href="mailto:tjhoyt7@gmail.com" class="social-link rounded-circle text-white mr-3" data-toggle="tooltip" data-placement="top" title="Email" data-original-title="Email">
-        <i class="icon fa fa-fw fa-envelope"></i>
+      <a href="mailto:tjhoyt7@gmail.com" className="social-link rounded-circle text-white mr-3" data-toggle="tooltip" data-placement="top" title="Email" data-original-title="Email">
+        <i className="icon fa fa-fw fa-envelope"></i>
       </a>
 
       {/* GitHub update href */}
       {/* <a href="" target="_blank">
-        <i class="icon fa fa-github mr-3"></i>
+        <i className="icon fa fa-github mr-3"></i>
       </a> */}
 
       {/* LinkedIn update href */}
-      {/* <a class="social-link rounded-circle text-white mr-3" data-toggle="tooltip" data-placement="top" target="_blank" title="LinkedIn" href="">
-        <i class="icon fa fa-linkedin" aria-hidden="true"></i>
+      {/* <a className="social-link rounded-circle text-white mr-3" data-toggle="tooltip" data-placement="top" target="_blank" title="LinkedIn" href="">
+        <i className="icon fa fa-linkedin" aria-hidden="true"></i>
       </a> */}
 
     </div>
     {/* Social icons   */}
 
     {/*Copyright  */}
-    <div class="footer-copyright">
-      <div class="container">
-        <small>Powered by <i class="fa fa-coffee" aria-hidden="true"></i> and <i class="fa fa-heart" aria-hidden="true"></i> Copyright &copy; Aidan Clemente & Paul Herman 2018</small>
+    <div className="footer-copyright">
+      <div className="container">
+        <small>Powered by <i className="fa fa-coffee" aria-hidden="true"></i> and <i className="fa fa-heart" aria-hidden="true"></i> Copyright &copy; Aidan Clemente & Paul Herman 2018</small>
       </div>
     </div>
     {/*/.Copyright  */}
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let state = {};
+window.setState =  (changes) => {
+    state = Object.assign({}, state, changes);
+
+    ReactDOM.render(<App {...state} />, document.getElementById("root"));
+}
+/* eslint no-restricted-globals: 0*/
+let initialState = {
+    name: "Aidan",
+    location: location.pathname.replace(/^\/?|\/$/g, ""),
+};
+
+window.setState(initialState);
+
+registerServiceWorker();
