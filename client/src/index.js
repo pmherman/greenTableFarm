6353561c041b33f0b82b5ad20de88c7a5284a30c
aidanclemente <aidanclemente@gmail.com>
2018-04-26 20:28:17 -0400

    undoing changes made in master

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from './registerServiceWorker';
 
-let state = {};
-window.setState =  (changes) => {
-    state = Object.assign({}, state, changes);
-
-    ReactDOM.render(<App {...state} />, document.getElementById("root"));
-}
-/* eslint no-restricted-globals: 0*/
-let initialState = {
-    name: "Aidan",
-    location: location.pathname.replace(/^\/?|\/$/g, ""),
-};
-
-window.setState(initialState);
-
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById("root"));
